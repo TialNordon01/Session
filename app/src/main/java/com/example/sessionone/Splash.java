65541486f0938314cd9cbbf2db700316e933d058
TialNordon01 <tialnordon@gmail.com>
2024-04-15 17:17:02 +0300

    splash 5s delay

--- app/src/main/java/com/example/sessionone/Splash.java
+++ app/src/main/java/com/example/sessionone/Splash.java
@@ -1,6 +1,10 @@
 package com.example.sessionone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Splash extends AppCompatActivity {
-
+    private Handler handler = new Handler(Looper.getMainLooper());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,16 @@ public class Splash extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Запускаем таймер на 5 секунд
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Переход на другую активность
+                Intent intent = new Intent(Splash.this, Onboarding1.class);
+                startActivity(intent);
+                finish(); // Завершаем текущую активность
+            }
+        }, 5000); // 5000 миллисекунд = 5 секунд
     }
 }
