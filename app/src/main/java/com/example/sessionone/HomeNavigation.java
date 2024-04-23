8d490e5f9af8c32b96b2ffcbef925c39d949d071
TialNordon01 <tialnordon@gmail.com>
2024-04-23 13:26:38 +0300

    navigation

--- app/src/main/java/com/example/sessionone/HomeNavigation.java
+++ app/src/main/java/com/example/sessionone/HomeNavigation.java
@@ -34,7 +34,7 @@ public class HomeNavigation extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_wallet, R.id.navigation_track)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_home_navigation);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
