0ca5bf062dab07a684c2c16f4afba15f05839cc0
TialNordon01 <tialnordon@gmail.com>
2024-04-23 17:10:37 +0300

    navigation profile

--- app/src/main/java/com/example/sessionone/Profile.java
+++ app/src/main/java/com/example/sessionone/Profile.java
@@ -1,8 +1,6 @@
 package com.example.sessionone;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Home extends AppCompatActivity {
+public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void Profile(View view) {
-        Intent intent = new Intent(this, Profile.class);
-        startActivity(intent);
     }
 }
